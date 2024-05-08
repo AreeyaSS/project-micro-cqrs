bb6b3bd54615ce01fdbe120c4e402a750853e9ac
AreeyaSS <103954008+AreeyaSS@users.noreply.github.com>
2024-05-08 15:02:05 +0700

    add orders service

---
+++ orders/src/orders/commands/handlers/create-user.handler.ts
@@ -0,0 +1,25 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CreateUserCommand } from '../impl/create-user.command';
+import { UserCreatedEvent } from 'src/orders/events/impl/user-created.event';
+import { StoreEventBus } from 'event-sourcing-nestjs';
+import { UidGenerator } from 'src/common/uid-generator';
+import { DateFactory } from 'src/common/date.factory';
+
+@CommandHandler(CreateUserCommand)
+export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
+
+    constructor(
+        private readonly eventBus: StoreEventBus,
+        private readonly uid: UidGenerator,
+        private readonly dateFactory: DateFactory,
+    ) {}
+
+    async execute(command: CreateUserCommand) {
+        const { name, email } = command;
+        // สร้าง UUIDs เพื่อควบคุมสถานะซ้ำ
+        const id = this.uid.generate();
+        this.eventBus.publish(new UserCreatedEvent(id, name, email, this.dateFactory.now()));
+        return id;
+    }
+
+}
